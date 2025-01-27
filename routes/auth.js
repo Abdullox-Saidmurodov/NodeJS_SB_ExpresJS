6018aca43bc42c3c5a03d0cc6e8511a1854e1e79
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-27 15:21:00 +0500

    9

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,17 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login', {
+        title: 'Login | AB',
+        isLogin: true,
+    })
 })
 
 router.get('/register', (req, res) => {
-    res.render('register')
+    res.render('register', {
+        title: 'Register | AB',
+        isRegister: true,
+    })
 })
 
 export default router
