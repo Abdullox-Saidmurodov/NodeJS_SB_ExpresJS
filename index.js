1340626e3ae6001630791cfe0a4a9c3ed0671f2a
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-28 11:03:56 +0500

    16

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import {create} from 'express-handlebars'
 import mongoose from 'mongoose'
 import flash from 'connect-flash'
 import session from 'express-session'
+import varMiddleware from './middleware/var.js'
+import cookieParser from 'cookie-parser'
 import * as dotenv from 'dotenv'
 
 // ROUTES
@@ -25,10 +27,10 @@ app.set('views', './views')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cookieParser())
 app.use(session({secret: "ab", resave: false, saveUninitialized: false}))
-// app.use(express.cookieParser('keyboard cat'))
-// app.use(express.session({ cookie: { maxAge: 60000 }}))
 app.use(flash())
+app.use(varMiddleware)
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
@@ -44,7 +46,5 @@ const startApp = () => {
         console.log(err)
     }
 }
-
-// console.log(process.env.MONGO_URI)
 
 startApp()
